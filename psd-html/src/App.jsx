d8f7640f0326dd7cb1324a5a9dc2c6c23539cd66
gulmina <gulmina44@gmail.com>
2024-06-10 12:01:36 +0200

    Future section added

--- psd-html/src/App.jsx
+++ psd-html/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import Footer from "./components/footer.jsx";
 
 function App() {
-  
-
   return (
-   <div>Hello World!</div>
-  )
+    <>
+      <div>Hello World!</div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 }
-
-export default App
+export default App;
---
+++ psd-html/src/components/footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./CSS/footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <img src="../img/5.png" alt="picture" />
+      <section className="future-section">
+        <div className="future-section-h1">DESIGN FOR</div>
+        <div className="future-section-h2">THE FUTURE</div>
+        <div className="future-section-p">
+          The focus is in creating excellent digital solutions that are clear,
+          user friendly, and aesthetically pleasing. The company gives
+          importance to come up with simple and clear flows and systems of
+          design for all users.
+        </div>
+      </section>
+      <footer>sdfhksj</footer>
+    </div>
+  );
+};
+export default Footer;
